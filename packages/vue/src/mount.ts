cd32b3f154abb0252556788e7c050cbb3f15c9e1
V.Symonenko <simonenko744@gmail.com>
2024-11-29 16:09:38 +0200

    fix(vue): set type second parameter of mount function

--- packages/vue/src/mount.ts
+++ packages/vue/src/mount.ts
@@ -1,9 +1,9 @@
 import { createApp, type Component } from 'vue';
 
-export function mount<T>(_cmp: Component<T>) {
+export function mount<T>(_cmp: Component<T>, props?: Record<string, unknown>) {
   if (!('setup' in _cmp)) {
     throw new Error(`${_cmp} instance is not Vue3 Component`);
   }
-  const app = createApp(_cmp);
+  const app = createApp(_cmp, props);
   return app.mount(document.createElement('div'));
 }
